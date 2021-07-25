1c0eb1d3e8cc0eb62c5a244987651b4673bd0417
PATRICIA <87294373+TEAM-PATRICIA@users.noreply.github.com>
2021-07-25 21:33:51 +0530

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-TITLE Masha Robot
-:: Enables virtual env mode and then starts Masha
-env\scripts\activate.bat && py -m MashaRoBot
+TITLE Patricia
+:: Enables virtual env mode and then starts Patricia
+env\scripts\activate.bat && py -m Patricia
