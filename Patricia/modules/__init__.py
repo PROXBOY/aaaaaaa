95f4aeddd3fe970e3731eab2f8c11413febaaa54
Bot-support <82792336+Bot-support@users.noreply.github.com>
2021-07-25 22:00:16 +0530

    Update __init__.py

--- Patricia/modules/__init__.py
+++ Patricia/modules/__init__.py
@@ -1,9 +1,9 @@
-from Patricia import LOAD, NO_LOAD, log
+from Patricia import LOAD, LOGGER, NO_LOAD
 
 
 def __list_all_modules():
-    from os.path import dirname, basename, isfile
     import glob
+    from os.path import basename, dirname, isfile
 
     # This generates a list of modules in this folder for the * in __main__ to work.
     mod_paths = glob.glob(dirname(__file__) + "/*.py")
@@ -20,7 +20,7 @@ def __list_all_modules():
                 any(mod == module_name for module_name in all_modules)
                 for mod in to_load
             ):
-                log.error("[KIGYO] Invalid loadorder names. Quitting.")
+                LOGGER.error("Invalid loadorder names. Quitting.")
                 quit(1)
 
             all_modules = sorted(set(all_modules) - set(to_load))
@@ -30,7 +30,7 @@ def __list_all_modules():
             to_load = all_modules
 
         if NO_LOAD:
-            log.info("[KIGYO] Not loading: {}".format(NO_LOAD))
+            LOGGER.info("Not loading: {}".format(NO_LOAD))
             return [item for item in to_load if item not in NO_LOAD]
 
         return to_load
@@ -39,5 +39,5 @@ def __list_all_modules():
 
 
 ALL_MODULES = __list_all_modules()
-log.info("[KIGYO] Modules to load: %s", str(ALL_MODULES))
+LOGGER.info("Modules to load: %s", str(ALL_MODULES))
 __all__ = ALL_MODULES + ["ALL_MODULES"]
